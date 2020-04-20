f6820f7056b2fe9a2651af1d6dc92d80a97678f0
wangshijun <wangshijun2010@gmail.com>
2020-04-20 08:20:22 +0800

    chore: cleanup did-auth-with-react code

--- examples/did-auth-with-react/src/index.js
+++ examples/did-auth-with-react/src/index.js
@@ -1,25 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { DidSessionProvider } from "@arcblock/did-connect/lib";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { DidSessionProvider } from '@arcblock/did-connect/lib';
 
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import './index.css';
+import App from './App';
 
 // Please change these to your own application settings
-const appDid = "zNKkYPct7wrDyZM3ZDB9P7FoBHWk8iipujyZ";
-const serviceHost = "http://192.168.1.5:4040";
+const appDid = 'zNKkYPct7wrDyZM3ZDB9P7FoBHWk8iipujyZ';
+const serviceHost = 'http://192.168.1.5:4040';
 
 // Required: Wrap the application with DidSessionProvider
 const WrappedApp = () => (
-  <DidSessionProvider appDid={appDid} serviceHost={serviceHost}>
+  <DidSessionProvider appDid={appDid} serviceHost={serviceHost} authAPIPrefix="/api/login/agent">
     <App />
   </DidSessionProvider>
 );
 
-ReactDOM.render(<WrappedApp />, document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<WrappedApp />, document.getElementById('root'));
